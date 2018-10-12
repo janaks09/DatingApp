62fcdd4bb12c3baf762c0563eb2c1d6e3cf4c481
Janak Shrestha <old_ink@hotmail.com>
2018-10-11 22:15:26 -0400

    End of section 19

--- Koseli.API/Helpers/AutoMapperProfiles.cs
+++ Koseli.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace Koseli.API.Helpers
                         opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                     });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
